f695e7bfedea018ff9e63aa84ae6aca19ce7752e
314581050謝采蓁 <127189671+poetry15@users.noreply.github.com>
2026-05-18 10:35:57 +0000

    add: make error in Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { buildApp } from './app';
 
-const app = buildApp({ logger: true });
-const port = Number(process.env.PORT || 3000);
-const host = process.env.HOST || '0.0.0.0';
+const app=buildApp({ logger: true });
+const port=Number(process.env.PORT || 3000);
+const host=process.env.HOST || '0.0.0.0';
 
 async function start() {
   try {
